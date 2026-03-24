b7bf23edfa11480d6ec0bc9506c332551b70db91
João Abdala <isacabala@gmail.com>
2026-03-24 15:36:57 +0000

    The jasmine lab solved

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,12 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("should return undefined if any argument is not a number", () => {
+        expect(add("a", "b")).toBeUndefined();
+        expect(add(2, "b")).toBeUndefined();
+        expect(add("a", 3)).toBeUndefined();
+      });
     });
   });
   
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,28 @@
-// UNCOMMENT THE CODE BELOW TO START
+describe("Function divide", () => {
 
-/*
+    it("should be defined", () => {
+        expect(divide).toBeDefined();
+    });
 
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+    it("should take to arguments", () => {
+        expect(divide.length).toBe(2);
+    });
 
-    })    
-})
+    it("sould return the division of two numbers", () => {
+        expect(divide(1, 2) ).toEqual(0.5);
+        expect(divide(3, 4) ).toEqual(0.75);
+        expect(divide(100, 50) ).toEqual(2);
+    });
 
-*/
+    it("sloud retunr undefined if any of the arguments is not provided", () => {
+        expect(divide(1) ).toEqual(undefined);
+        expect(divide() ).toEqual(undefined);
+        expect(divide(undefined, 1) ).toEqual(undefined);
+    });
+
+    it("should return undefined if any argument is not a number", () => {
+        expect(divide("a", "b")).toBeUndefined();
+        expect(divide(2, "b")).toBeUndefined();
+        expect(divide("a", 3)).toBeUndefined();
+    });
+});
